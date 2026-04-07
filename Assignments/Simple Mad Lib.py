146cf40c2585462076834120fea2618059a0ea10
Benard395 <824226@mystma.org>
2026-04-07 14:30:55 -0500

    spaceshuttle

---
+++ Assignments/Simple Mad Lib.py
@@ -0,0 +1,21 @@
+# Once upon a time, in a [adjective] [noun],
+# there lived a [adjective] [noun].
+# Every Day, the [noun] would [verb] [adverb].
+# One day, a [adjective] [noun] appread
+# and [verb] the [noun] away.
+# The [noun] was [adjective] and [emotion].
+Adjective1 = input("Give an adjective")
+Noun1 = input("Give a noun")
+Adjective2 = input("Give and adjective")
+Noun2 = input("Give a noun")
+Noun3 = input("Give a noun")
+Verb1 = input("Give a verb")
+Adverb1 = input("Give a adverb")
+Adjective3 = input("Give a adjective")
+Noun4 = input("Give a noun")
+Verb2 = input("Give a verb")
+Noun5 = input("Give a noun")
+Noun6 = input("Give a noun")
+Adjective4 = input("Give a adjective")
+Emotion1 = input("Give an emotion")
+print("Once upon a time, in a [Adjective1] [Noun1], there lived a [Adjective2] [Noun2]. Every day, the [Noun3] would [Verb1] [Adverb1]. One day, a [Adjective3] [Noun4] appeared and [Verb2] the [Noun5] away. The [Noun6] was [Adjective4] and [Emotion1]")
